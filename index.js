5e0f4c59aa6921cf9290b69fccb381f24db73efb
Evan Halley <evan.halley@archivesocial.com>
2019-04-29 16:33:46 -0400

    implemented part of the API

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const PORT = 8080;
+const CODELAB_URL = 'https://codelabs.developers.google.com/';
+
+let app = express();
+
+app.get('/codelab', async (req, res) => {
+    let key, value;
+
+    if (req.param.title) {
+        key = 'title';
+        value = req.param.title;
+    } else if (req.param.duration) {
+        key = 'duration';
+        value = req.param.duration;
+    } else if (req.param.category) {
+        key = 'category';
+        value = req.param.category;
+    }
+
+    let results = {
+        title: 'This is just a title',
+        duration: '79',
+        category: 'IoT',
+        url: 'http://google.com/linktocodelab'
+    };
+    res.json(results);
+});
+
+async function search(query) {
+
+}
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
