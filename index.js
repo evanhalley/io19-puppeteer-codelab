4d36bbfbdb5efbcfee5825249dccf1e4b9fc0789
Evan Halley <evan.halley@gmail.com>
2019-05-02 06:42:57 -0400

    added filtering logic

--- index.js
+++ index.js
@@ -9,30 +9,46 @@ const CODELAB_URL = 'https://codelabs.developers.google.com/';
 let app = express();
 
 app.get('/codelab', async (req, res) => {
-    let key, value;
+    let filter, query;
 
-    if (req.param.title) {
-        key = 'title';
-        value = req.param.title;
-    } else if (req.param.duration) {
-        key = 'duration';
-        value = req.param.duration;
-    } else if (req.param.category) {
-        key = 'category';
-        value = req.param.category;
+    if (req.query.title) {
+        filter = 'title';
+        query = req.query.title;
+    } else if (req.query.duration_gt) {
+        filter = 'duration';
+        query = req.query.duration;
+    }  else if (req.query.duration_lt) {
+        filter = 'duration';
+        query = req.query.duration;
+    } else if (req.query.category) {
+        filter = 'category';
+        query = req.query.category;
     }
-
-    let results = {
-        title: 'This is just a title',
-        duration: '79',
-        category: 'IoT',
-        url: 'http://google.com/linktocodelab'
-    };
+    let results = await search({ filter: filter, query: query })
     res.json(results);
 });
 
 async function search(query) {
+    let results = [];
+    
+    // TODO: use puppeteer to get the codelabs from the codelabs webpage
 
+    results.push({"title":"This is just a title","duration":"79","category":"IoT","url":"http://google.com/linktocodelab"});
+
+    // filter and return results
+    return results.filter(codelab => {
+
+        if (query.filter && query.query) {
+            if (query.filter === 'title' || query.filter === 'category') {
+                return codelab[query.filter].toLowerCase().indexOf(query.query.toLowerCase()) > -1;
+            } else if (query.filter === 'duration_lt') {
+                return codelab['duration'] < query.query;
+            } else if (query.filter === 'duration_gt') {
+                return codelab['duration'] > query.query;
+            }
+        }
+        return true;
+    });
 }
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
